93ce5432a460c0142c88cfa354ad9b5cfad8d407
Qurstron <46225239+Qurstron@users.noreply.github.com>
2026-09-25 12:13:39 +0300

    Change DataSeeder to only be effective on dev build Add admin user

--- backend/src/main/java/com/example/hitbloqproxy/DataSeeder.java
+++ backend/src/main/java/com/example/hitbloqproxy/DataSeeder.java
@@ -1,8 +1,11 @@
 package com.example.hitbloqproxy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.hitbloqproxy.apikey.UserApiKeyService;
+import com.example.hitbloqproxy.auth.RegistrationService;
 import com.example.hitbloqproxy.beatmap.BeatMap;
 import com.example.hitbloqproxy.beatmap.BeatMapRepository;
 import com.example.hitbloqproxy.pool.Pool;
@@ -11,30 +14,42 @@ import com.example.hitbloqproxy.poolentry.PoolBeatMapEntryService;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
+    @Value("${app.test-data.enabled}")
+    private boolean enabled;
 
     private final BeatMapRepository beatMapRepository;
     private final PoolRepository poolRepository;
     private final PoolBeatMapEntryService poolBeatMapEntryService;
+    private final RegistrationService registrationService;
+    private final UserApiKeyService userApiKeyService;
 
-    public DataSeeder(BeatMapRepository beatMapRepository, PoolRepository poolRepository, PoolBeatMapEntryService poolBeatMapEntryService) {
+    public DataSeeder(BeatMapRepository beatMapRepository, PoolRepository poolRepository, PoolBeatMapEntryService poolBeatMapEntryService, RegistrationService registrationService, UserApiKeyService userApiKeyService) {
         this.beatMapRepository = beatMapRepository;
         this.poolRepository = poolRepository;
         this.poolBeatMapEntryService = poolBeatMapEntryService;
+        this.registrationService = registrationService;
+        this.userApiKeyService = userApiKeyService;
     }
 
     @Override
     public void run(String... args) {
-        if (beatMapRepository.count() == 0) {
+        if (enabled) {
             BeatMap beatMap1 = new BeatMap();
+            String pool1name = "Example Pool";
+            String email1 = "a@a.com";
+
             beatMap1.setHitbloqId("example-hitbloq-id");
             beatMap1.setName("Example BeatMap");
             beatMapRepository.save(beatMap1);
 
             Pool pool1 = new Pool();
-            pool1.setName("Example Pool");
+            pool1.setName(pool1name);
             poolRepository.save(pool1);
 
             poolBeatMapEntryService.addBeatMapToPool(beatMap1.getId(), pool1.getId());
+
+            registrationService.register(email1, "admin");
+            userApiKeyService.create(email1, pool1name, "TEST-API-KEY-TEST");
         }
     }
 }
